86562506f2070a656739b2097045991ca8f41b70
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-04-20 21:52:11 +0530

    added Employee entity

---
+++ src/main/java/com/rest_apis/employee_crud/entities/Employee.java
@@ -0,0 +1,52 @@
+package com.rest_apis.employee_crud.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int empId;
+	private String firstName;
+	private String lastName;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(int empId, String firstName, String lastName) {
+		super();
+		this.empId = empId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}

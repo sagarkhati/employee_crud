8521042b840ff27928583cc81707c9ec9c32796d
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-04-20 21:59:20 +0530

    added data script and required properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into employee (emp_id, first_name, last_name) values (1, 'Jack', 'Rayan');
+insert into employee (emp_id, first_name, last_name) values (2, 'Jack', 'Reacher');
+insert into employee (emp_id, first_name, last_name) values (3, 'James', 'Bond');
